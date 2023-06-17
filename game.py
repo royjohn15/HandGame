f8ea104e4aa32f45fdf6e29dcd73b948cc08d254
royjohn15 <royjohnabc@gmail.com>
2023-06-17 20:58:55 +0530

    finished game

--- game.py
+++ game.py
@@ -1,41 +1,106 @@
+import pygame
+from pygame.locals import *
+import random
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-import time
-import random
+import threading
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8, maxHands=1)
+detector = HandDetector(detectionCon = 0.8, maxHands = 1)
 
 
+width, height = (500, 800)
+road_w = int(width/1.6)
+roadmark_w = int(width/80)
+right_lane = width/2 + road_w/4
+left_lane = width/2 - road_w/4
+speed = 1
 
-def generate_frames():
-		score = 0
-		last_time = time.time()  # Variable to keep track of the last time the random number was generated
-		while True:
-			success, frame = cap.read()  # get image frame
-			if not success:
-			    break
-			else:
-			    hands, frame = detector.findHands(frame)  # detect hand
+min_x = int(width/2 - road_w/2) + 64  # Minimum x-coordinate allowed
+max_x = int(width/2 + road_w/2) - 250 # Maximum x-coordinate allowed
 
-			    if hands:
-			        hand = hands[0]
-			        hand_type = hand["type"]
-			        fingers = detector.fingersUp(hand)
 
-			        current_time = time.time()  # Get the current time
+pygame.init()
+running = True
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Hand Car Game")
+screen.fill((60,220,0))
+pygame.display.update()
 
-			        if current_time - last_time >= 3:  # Check if 3 seconds have passed since the last random number generation
-			            random_number = random.randint(1, 6)  # Generate a random number from 1 to 6
-			            print("Fingers:", fingers, "Random Number:", random_number, "Score:", score)
-			            if random_number == sum(fingers):
-			                print("game over")
-			            else:
-			                score += sum(fingers)
-			            last_time = current_time  # Update the last time variable
+car = pygame.image.load("car.png")
+car_loc = car.get_rect()
+car_loc.center = right_lane, height*0.8
 
-			    ret, buffer = cv2.imencode('.jpg', frame)
-			    frame = buffer.tobytes()
+enemy = pygame.image.load("enemy.png")
+enemy_loc = enemy.get_rect()
+enemy_loc.center = left_lane, height*0.2
 
-			    yield (b'--frame\r\n'
-			           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+counter = 0
+while running:
+	counter += 1
+	if counter == 10:
+		speed += 0.15
+		counter = 0
+
+	enemy_loc[1] += speed
+
+	if enemy_loc[1] > height:
+		if random.randint(0,1) == 0:
+			enemy_loc.center = right_lane, -200
+		else:
+			enemy_loc.center = left_lane, -200
+
+	if car_loc[0] == enemy_loc[0] and enemy_loc[1] >= car_loc[1] - 128:
+		print("Game Over")
+		break
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			running = False
+		if event.type == KEYDOWN:
+			if event.key in [K_a, K_LEFT]:
+				car_loc = car_loc.move([-int(road_w/2), 0])
+			if event.key in [K_d, K_RIGHT]:
+				car_loc = car_loc.move([int(road_w/2), 0])
+
+	pygame.draw.rect(
+		screen,
+		(50,50,50),
+		(width/2 - road_w/2, 0, road_w, height)
+		)
+
+	pygame.draw.rect(
+		screen,
+		(255,240,60),
+		(width/2 - roadmark_w/2, 0, roadmark_w, height)
+		)
+
+	pygame.draw.rect(
+		screen,
+		(255,255,255),
+		(width/2-road_w/2 + roadmark_w * 2, 0, roadmark_w, height)
+		)
+
+	pygame.draw.rect(
+		screen,
+		(255,255,255),
+		(width/2+road_w/2 - roadmark_w * 3, 0, roadmark_w, height)
+		)
+	screen.blit(car, car_loc)
+	screen.blit(enemy, enemy_loc)
+	pygame.display.update()
+
+	success, frame = cap.read()
+	hands, frame = detector.findHands(frame)
+	if hands:
+		hand = hands[0]
+		fingers = detector.fingersUp(hand)
+
+		if sum(fingers) == 5:
+			if car_loc[0] > min_x:
+				car_loc = car_loc.move([-int(road_w/2), 0])
+		if sum(fingers) == 0:
+			if car_loc[0] < max_x:
+				car_loc = car_loc.move([int(road_w/2), 0])
+	cv2.imshow("Image",frame)
+pygame.quit()
--- main.py
+++ main.py
@@ -1,16 +1,122 @@
-from flask import Flask, render_template, Response
-from game import generate_frames
+import pygame
+from pygame.locals import *
+import random
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import threading
 
-app = Flask(__name__)
+width, height = (500, 800)
+road_w = int(width/1.6)
+roadmark_w = int(width/80)
+right_lane = width/2 + road_w/4 
+left_lane = width/2 - road_w/4
+speed = 1
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+min_x = int(width/2 - road_w/2) + 64  # Minimum x-coordinate allowed
+max_x = int(width/2 + road_w/2) - 250 # Maximum x-coordinate allowed
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# Initialize Pygame
+pygame.init()
+running = True
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Hand Car Game")
+screen.fill((60,220,0))
+pygame.display.update()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Create the car and enemy objects, and set their initial positions
+car = pygame.image.load("car.png")
+car_loc = car.get_rect()
+car_loc.center = right_lane, height * 0.8
 
+enemy = pygame.image.load("enemy.png")
+enemy_loc = enemy.get_rect()
+enemy_loc.center = left_lane, height * 0.2
+
+def process_frames():
+    global running, car_loc, road_w, min_x, max_x
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(detectionCon=0.8, maxHands=1)
+
+    while running:
+        success, frame = cap.read()
+        hands, frame = detector.findHands(frame)
+
+        if hands:
+            hand = hands[0]
+            fingers = detector.fingersUp(hand)
+
+            if sum(fingers) == 5:
+                if car_loc[0] > min_x:
+                    car_loc = car_loc.move([-int(road_w/2), 0])
+            elif sum(fingers) == 0:
+                if car_loc[0] < max_x:
+                    car_loc = car_loc.move([int(road_w/2), 0])
+
+        cv2.imshow("Image", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Start the OpenCV frame processing thread
+thread = threading.Thread(target=process_frames)
+thread.start()
+
+# Game loop
+counter = 0
+while running:
+    counter += 1
+    if counter == 5000:
+        speed += 0.15
+        counter = 0
+
+    enemy_loc[1] += speed
+
+    if enemy_loc[1] > height:
+        if random.randint(0,1) == 0:
+            enemy_loc.center = right_lane, -200
+        else:
+            enemy_loc.center = left_lane, -200
+
+    if car_loc[0] == enemy_loc[0] and enemy_loc[1] >= car_loc[1] - 128:
+        print("Game Over")
+        break
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+        if event.type == KEYDOWN:
+            if event.key in [K_a, K_LEFT]:
+                car_loc = car_loc.move([-int(road_w/2), 0])
+            if event.key in [K_d, K_RIGHT]:
+                car_loc = car_loc.move([int(road_w/2), 0])
+
+    pygame.draw.rect(
+        screen,
+        (50,50,50),
+        (width/2 - road_w/2, 0, road_w, height)
+        )
+
+    pygame.draw.rect(
+        screen,
+        (255,240,60),
+        (width/2 - roadmark_w/2, 0, roadmark_w, height)
+        )
+
+    pygame.draw.rect(
+        screen,
+        (255,255,255),
+        (width/2-road_w/2 + roadmark_w * 2, 0, roadmark_w, height)
+        )
+
+    pygame.draw.rect(
+        screen,
+        (255,255,255),
+        (width/2+road_w/2 - roadmark_w * 3, 0, roadmark_w, height)
+        )
+    screen.blit(car, car_loc)
+    screen.blit(enemy, enemy_loc)
+    pygame.display.update()
+pygame.quit()
